69e4e8d85b6ca270157bff932450bbe259a762ca
Matt Beton <matthew.beton@gmail.com>
2017-07-30 23:00:45 +0100

    Added Insert and GetBlock to SQLConnector

--- BlockExplorer/SqlConnector.cs
+++ BlockExplorer/SqlConnector.cs
@@ -2,6 +2,7 @@
 using MySql.Data.MySqlClient;
 using NBitcoin;
 using System;
+using System.Text;
 
 namespace NodeConnector
 {
@@ -34,12 +35,34 @@ namespace NodeConnector
 
 		internal void InsertBlock(BlockS b)
 		{
+			string query = "INSERT INTO block (" +
+				"height, hash, version, merkleroot, time, nonce, bits, " +
+				"size, strippedsize, weight, chainwork, nextblockhash, versionhex" +
+				") VALUES('" +
+				b.height + "', " +
+				"unhex('" + b.hash + "'), '" +
+				b.version + "', " +
+				"unhex('" + b.merkleroot + "'), '" +
+				b.time + "', '" +
+				b.nonce + "', " +
+				"unhex('" + b.bits + "'), '" +
+				b.size + "', '" +
+				b.strippedsize + "', '" +
+				b.weight + "', " +
+				"unhex('" + b.chainwork + "'), " +
+				"unhex('" + b.nextblockhash + "'), " +
+				"unhex('" + b.versionhex + "'));";
 
+				//Create Command
+				MySqlCommand cmd = new MySqlCommand(query, connection);
+				//Execute the command
+
+				 cmd.ExecuteReader();
 		}
 
 		public Block GetBlock(int height)
 		{
-			string query = "SELECT * FROM block WHERE height = " + height + ";";
+			string query = "SELECT * FROM blockchain.block WHERE height = " + height + ";";
 
 			//Create Command
 			MySqlCommand cmd = new MySqlCommand(query, connection);
@@ -49,16 +72,41 @@ namespace NodeConnector
 
 			//Read the data and store them in the Block
 			Block block = new Block();
-// if (dataReader.Depth > 1) TODO: Break if there is more than 1 block with the same height.
 			while (dataReader.Read())
 			{
-				BlockHeader b = new BlockHeader();
-				b.Bits = new Target(new uint256(dataReader["bits"].ToString()));
+				BlockS b = new BlockS();
+				
+				b.height = Convert.ToInt32(dataReader.GetString("height"));
+				Console.WriteLine(ToHex(dataReader.GetString("hash")));
+				Console.WriteLine(dataReader.GetString("hash").Length);
+				var v = ToHex(dataReader.GetString("hash"));
+				b.hash = new uint256(ToHex(dataReader.GetString("hash")));
+				b.version = Convert.ToInt32(dataReader.GetString("version"));
+				b.merkleroot = new uint256(ToHex(dataReader.GetString("merkleroot")));
+				b.time = Convert.ToUInt32(dataReader.GetString("time"));
+				b.nonce = Convert.ToUInt32(dataReader.GetString("nonce"));
+				b.bits = Convert.ToUInt32(ToHex(dataReader.GetString("bits")));
+				b.size = Convert.ToUInt32(dataReader.GetString("size"));
+				b.strippedsize = Convert.ToUInt32(dataReader.GetString("strippedsize"));
+				b.weight = Convert.ToUInt32(dataReader.GetString("weight"));
+				b.chainwork = new uint256(ToHex(dataReader.GetString("chainwork")));
+				b.nextblockhash = new uint256(ToHex(dataReader.GetString("nextblockhash")));
+				b.versionhex = Convert.ToUInt32(ToHex(dataReader.GetString("versionhex")));
 			}
 
 			dataReader.Close();
 
 			return null;
+		}
+
+		private String ToHex(String i_asciiText)
+		{
+			StringBuilder sBuffer = new StringBuilder();
+			for (int i = 0; i < i_asciiText.Length; i++)
+			{
+				sBuffer.Append(Convert.ToInt32(i_asciiText[i]).ToString("x"));
+			}
+			return sBuffer.ToString().ToUpper();
 		}
 	}
 }
